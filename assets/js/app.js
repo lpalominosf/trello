eb188c6a582355f61913a78cd702caa9eb29b410
lpalominosf <lorena.palominos@gmail.com>
2017-11-07 22:10:18 -0300

    subiendo avances

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+/* 1.- crear div vacio para que contenga cada lista 
+	2.- crear un text area que contenga los caracteres ingresados 
+*/
+
+
+
+
+var message = getElementById(txt);
+function () {
+	
+}
