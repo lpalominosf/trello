788eac3f9d23239984740290855995e7ad177f3f
lpalominosf <lorena.palominos@gmail.com>
2017-11-11 12:29:46 -0300

    agregando (intentando) vista en varias pantallas

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,8 @@
-/* 1.- crear div vacio para que contenga cada lista 
-	2.- crear un text area que contenga los caracteres ingresados 
+/*  1.- crear div vacio para que contenga cada lista 
+	2.- crear eventos de click para cada nueva interaccion:
+		click que abra input + botones 
+		click que permita guardar la lista en un div nuevo
+
 */
 
 
